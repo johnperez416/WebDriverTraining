e731a02ce715d42746a3f29cda2f0ad3541635d1
Matthew Casperson <matthewcasperson@gmail.com>
2018-10-06 08:49:47 +1000

    Post 24

--- src/test/java/com/octopus/TicketMonsterTest.java
+++ src/test/java/com/octopus/TicketMonsterTest.java
@@ -95,4 +95,52 @@ public class TicketMonsterTest {
             automatedBrowser.destroy();
         }
     }
+
+    @Test
+    public void verifyPricesPageObjectModel() {
+
+        final AutomatedBrowser automatedBrowser =
+                AUTOMATED_BROWSER_FACTORY.getAutomatedBrowser("ChromeNoImplicitWait");
+
+        try {
+            automatedBrowser.init();
+
+            final EventsPage eventsPage = new MainPage(automatedBrowser)
+                    .openPage()
+                    .buyTickets();
+
+            final VenuePage venuePage = eventsPage
+                    .selectEvent("Theatre", "Shane's Sock Puppets");
+
+            final CheckoutPage checkoutPage = venuePage
+                    .selectVenue("Toronto : Roy Thomson Hall")
+                    .book();
+
+            checkoutPage.selectSection("A - Premier platinum reserve");
+
+            final float platinumAdultPrices = checkoutPage.getSectionAdultPrices();
+            Assert.assertTrue(platinumAdultPrices > 10);
+            Assert.assertTrue(platinumAdultPrices < 1000);
+
+            checkoutPage.selectSection("B - Premier gold reserve");
+
+            final float goldAdultPrices = checkoutPage.getSectionAdultPrices();
+            Assert.assertTrue(goldAdultPrices > 10);
+            Assert.assertTrue(goldAdultPrices < 1000);
+
+            checkoutPage.selectSection("C - Premier silver reserve");
+
+            final float silverAdultPrices = checkoutPage.getSectionAdultPrices();
+            Assert.assertTrue(silverAdultPrices > 10);
+            Assert.assertTrue(silverAdultPrices < 1000);
+
+            checkoutPage.selectSection("D - General");
+
+            final float generalAdultPrices = checkoutPage.getSectionAdultPrices();
+            Assert.assertTrue(generalAdultPrices > 10);
+            Assert.assertTrue(generalAdultPrices < 1000);
+        } finally {
+            automatedBrowser.destroy();
+        }
+    }
 }
--- src/test/java/com/octopus/pages/ticketmonster/CheckoutPage.java
+++ src/test/java/com/octopus/pages/ticketmonster/CheckoutPage.java
@@ -4,6 +4,9 @@ import com.octopus.AutomatedBrowser;
 
 import com.octopus.pages.BasePage;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CheckoutPage extends BasePage {
 
     private static final String SECTION_DROP_DOWN_LIST = "sectionSelect";
@@ -11,11 +14,22 @@ public class CheckoutPage extends BasePage {
     private static final String ADD_TICKETS_BUTTON = "add";
     private static final String EMAIL_ADDRESS = "email";
     private static final String CHECKOUT_BUTTON = "submit";
+    private static final Pattern TICKET_PRICE_REGEX = Pattern.compile("@ \\$(\\d+\\.\\d+)");
+    private static final String ADULT_TICKET_PRICE = ".input-group-addon";
+
 
     public CheckoutPage(final AutomatedBrowser automatedBrowser) {
-
         super(automatedBrowser);
+    }
 
+    public CheckoutPage selectSection(final String section) {
+        automatedBrowser.selectOptionByTextFromSelect(section, SECTION_DROP_DOWN_LIST, WAIT_TIME);
+        return this;
+    }
+
+    public float getSectionAdultPrices() {
+        final String price = automatedBrowser.getTextFromElement(ADULT_TICKET_PRICE);
+        return getPriceFromTicketPriceString(price);
     }
 
     public CheckoutPage buySectionTickets(final String section, final
@@ -30,5 +44,15 @@ public class CheckoutPage extends BasePage {
         automatedBrowser.populateElement(EMAIL_ADDRESS, email, WAIT_TIME);
         automatedBrowser.clickElement(CHECKOUT_BUTTON, WAIT_TIME);
         return new ConfirmationPage(automatedBrowser);
+    }
+
+    private float getPriceFromTicketPriceString(final String input) {
+        final Matcher matcher = TICKET_PRICE_REGEX.matcher(input);
+        if (matcher.matches()) {
+            final String priceString = matcher.group(1);
+            return Float.parseFloat(priceString);
+        }
+
+        throw new IllegalArgumentException("String " + input + " does not match the regex");
     }
 }
