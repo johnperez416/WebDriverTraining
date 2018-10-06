75883e8d12e92038676ddcdaf6a88e8ae90dc79b
Matthew Casperson <matthewcasperson@gmail.com>
2018-10-07 07:18:35 +1000

    Working on eport uploads

--- src/main/java/com/octopus/LambdaEntry.java
+++ src/main/java/com/octopus/LambdaEntry.java
@@ -1,58 +1,95 @@
 package com.octopus;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
 import com.amazonaws.services.simpleemail.model.*;
+import com.octopus.utils.ZipUtils;
+import com.octopus.utils.impl.ZipUtilsImpl;
 import org.apache.commons.io.FileUtils;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+import java.util.UUID;
+
+class LambdaInput {
+    private String id;
+    private String feature;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(final String id) {
+        this.id = id;
+    }
+
+    public String getFeature() {
+        return feature;
+    }
+
+    public void setFeature(final String feature) {
+        this.feature = feature;
+    }
+}
 
 public class LambdaEntry {
+    private static final ZipUtils ZIP_UTILS = new ZipUtilsImpl();
     private static final String CHROME_HEADLESS_PACKAGE =
             "https://s3.amazonaws.com/webdriver-testing-resources/stable-headless-chromium-amazonlinux-2017-03.zip";
     private static final String CHROME_DRIVER =
             "https://s3.amazonaws.com/webdriver-testing-resources/chromedriver_linux64.zip";
 
-    public String runCucumber(String feature) throws Throwable {
+    public String runCucumber(final LambdaInput input) throws Throwable {
+        System.out.println("STARTED Cucumber Test ID " + input.getId());
 
         File driverDirectory = null;
         File chromeDirectory = null;
         File outputFile = null;
         File txtOutputFile = null;
         File featureFile = null;
+        File htmlOutput = null;
 
         try {
             driverDirectory = downloadChromeDriver();
             chromeDirectory = downloadChromeHeadless();
             outputFile = Files.createTempFile("output", ".json").toFile();
             txtOutputFile = Files.createTempFile("output", ".txt").toFile();
-            featureFile = writeFeatureToFile(feature);
+            featureFile = writeFeatureToFile(input.getFeature());
+            htmlOutput = Files.createTempDirectory("htmloutput").toFile();
 
-            cucumber.api.cli.Main.run(
+            final int retValue = cucumber.api.cli.Main.run(
                     new String[]{
                             "--monochrome",
                             "--glue", "com.octopus.decoratorbase",
-                            "--format", "json:" + outputFile.toString(),
-                            "--format", "pretty:" + txtOutputFile.toString(),
+                            "--plugin", "json:" + outputFile.toString(),
+                            "--plugin", "pretty:" + txtOutputFile.toString(),
+                            "--plugin", "html:" + htmlOutput.toString(),
                             featureFile.getAbsolutePath()},
                     Thread.currentThread().getContextClassLoader());
 
+            System.out.println((retValue == 0 ? "SUCCEEDED" : "FAILED") + " Cucumber Test ID " + input.getId());
+
+            uploadS3Report(input.getId(), htmlOutput.getAbsolutePath(), "us-east-1", "cucumber-report-files");
             sendEmail("admin@matthewcasperson.com", FileUtils.readFileToString(txtOutputFile, Charset.defaultCharset()));
 
             return FileUtils.readFileToString(outputFile, Charset.defaultCharset());
-
         } finally {
             FileUtils.deleteQuietly(driverDirectory);
             FileUtils.deleteQuietly(chromeDirectory);
             FileUtils.deleteQuietly(outputFile);
             FileUtils.deleteQuietly(txtOutputFile);
             FileUtils.deleteQuietly(featureFile);
+            FileUtils.deleteQuietly(htmlOutput);
+
+            System.out.println("FINISHED Cucumber Test ID " + input.getId());
         }
     }
 
@@ -78,33 +115,12 @@ public class LambdaEntry {
             downloadedFile = File.createTempFile("download", ".zip");
             FileUtils.copyURLToFile(new URL(download), downloadedFile);
             final File extractedDir = Files.createTempDirectory(tempDirPrefix).toFile();
-            unzipFile(downloadedFile.getAbsolutePath(), extractedDir.getAbsolutePath());
+            ZIP_UTILS.unzipFile(downloadedFile.getAbsolutePath(), extractedDir.getAbsolutePath());
             return extractedDir;
         } finally {
             FileUtils.deleteQuietly(downloadedFile);
         }
 
-    }
-
-    private void unzipFile(final String fileZip, final String outputDirectory) throws IOException {
-
-        final byte[] buffer = new byte[1024];
-
-        try (final ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip))) {
-            ZipEntry zipEntry = zis.getNextEntry();
-            while (zipEntry != null) {
-                final String fileName = zipEntry.getName();
-                final File newFile = new File(outputDirectory + "/" + fileName);
-                try (final FileOutputStream fos = new FileOutputStream(newFile)) {
-                    int len;
-                    while ((len = zis.read(buffer)) > 0) {
-                        fos.write(buffer, 0, len);
-                    }
-                }
-                zipEntry = zis.getNextEntry();
-            }
-            zis.closeEntry();
-        }
     }
 
     private File writeFeatureToFile(final String feature) throws IOException {
@@ -138,6 +154,40 @@ public class LambdaEntry {
             client.sendEmail(request);
         } catch (final Exception ex) {
             System.out.println("The email was not sent. Error message: " + ex.getMessage());
+        }
+    }
+
+    private static void uploadS3Report(
+            final String id,
+            final String reportDir,
+            final String clientRegion,
+            final String bucketName) {
+        final String fileObjKeyName = "htmlreport" + UUID.randomUUID() + ".zip";
+        
+        File report = null;
+
+        try {
+            report =  File.createTempFile("htmlreport", ".zip");
+            ZIP_UTILS.zipDirectory(report.getAbsolutePath(), reportDir);
+
+            final AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withRegion(clientRegion)
+                    .withCredentials(new ProfileCredentialsProvider())
+                    .build();
+
+            // Upload a file as a new object with ContentType and title specified.
+            final PutObjectRequest request = new PutObjectRequest(bucketName, fileObjKeyName, report);
+            final ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType("application/zip");
+            metadata.addUserMetadata("x-amz-meta-title", "Cucumber Report");
+            request.setMetadata(metadata);
+            s3Client.putObject(request);
+
+            System.out.println("UPLOADED Cucumber Test ID " + id + " to " + fileObjKeyName);
+        } catch(final Exception ex) {
+            System.out.println("The report was not uploaded to S3. Error message: " + ex.getMessage());
+        } finally {
+            FileUtils.deleteQuietly(report);
         }
     }
 }
---
+++ src/main/java/com/octopus/utils/impl/ZipUtilsImpl.java
@@ -0,0 +1,72 @@
+package com.octopus.utils.impl;
+
+import com.octopus.utils.ZipUtils;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+public class ZipUtilsImpl implements ZipUtils {
+    public void unzipFile(final String fileZip, final String outputDirectory) throws IOException {
+
+        final byte[] buffer = new byte[1024];
+
+        try (final ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip))) {
+            ZipEntry zipEntry = zis.getNextEntry();
+            while (zipEntry != null) {
+                final String fileName = zipEntry.getName();
+                final File newFile = new File(outputDirectory + "/" + fileName);
+                try (final FileOutputStream fos = new FileOutputStream(newFile)) {
+                    int len;
+                    while ((len = zis.read(buffer)) > 0) {
+                        fos.write(buffer, 0, len);
+                    }
+                }
+                zipEntry = zis.getNextEntry();
+            }
+            zis.closeEntry();
+        }
+    }
+
+    public void zipDirectory(final String fileZip, final String inputDirectory) throws IOException {
+        final File inputDirectoryFile = new File(inputDirectory);
+        final byte[] buffer = new byte[1024];
+
+        FileOutputStream fos = null;
+        ZipOutputStream zos = null;
+
+        try {
+            fos = new FileOutputStream(fileZip);
+            zos = new ZipOutputStream(fos);
+
+            FileInputStream in = null;
+
+            for (final File file: FileUtils.listFiles(inputDirectoryFile, null,true)) {
+                final ZipEntry ze = new ZipEntry(file.getPath());
+                zos.putNextEntry(ze);
+                try {
+                    in = new FileInputStream(file.getPath());
+                    int len;
+                    while ((len = in .read(buffer)) > 0) {
+                        zos.write(buffer, 0, len);
+                    }
+                } finally {
+                    in.close();
+                }
+            }
+
+            zos.closeEntry();
+        } finally {
+            try {
+                zos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
