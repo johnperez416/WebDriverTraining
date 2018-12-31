817143200449f5f95465e38f18319e0dbc84fa88
Matthew Casperson <matthewcasperson@gmail.com>
2018-12-31 17:01:04 +1000

    Force each Chrome run to use unique dirs

--- src/main/java/com/octopus/decorators/ChromeHeadlessLambdaDecorator.java
+++ src/main/java/com/octopus/decorators/ChromeHeadlessLambdaDecorator.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.UUID;
+
 public class ChromeHeadlessLambdaDecorator extends AutomatedBrowserBase
 {
     public ChromeHeadlessLambdaDecorator(final AutomatedBrowser automatedBrowser) {
@@ -21,10 +23,10 @@ public class ChromeHeadlessLambdaDecorator extends AutomatedBrowserBase
         options.addArguments("--window-size=1366,768");
         options.addArguments("--single-process");
         options.addArguments("--no-sandbox");
-        options.addArguments("--user-data-dir=/tmp/user-data");
-        options.addArguments("--data-path=/tmp/data-path");
-        options.addArguments("--homedir=/tmp");
-        options.addArguments("--disk-cache-dir=/tmp/cache-dir");
+        options.addArguments("--user-data-dir=/tmp/user-data" + UUID.randomUUID());
+        options.addArguments("--data-path=/tmp/data-path" + UUID.randomUUID());
+        options.addArguments("--homedir=/tmp" + UUID.randomUUID());
+        options.addArguments("--disk-cache-dir=/tmp/cache-dir" + UUID.randomUUID());
 
         if (System.getProperty("chrome.binary") != null) {
             options.setBinary(System.getProperty("chrome.binary"));
