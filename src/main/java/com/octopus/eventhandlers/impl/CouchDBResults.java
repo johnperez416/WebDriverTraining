a78610960d3a10de31305430b8e0a3bb74c432e9
Matthew Casperson <matthewcasperson@gmail.com>
2019-01-18 16:14:32 +1000

    Fixed up the conflicts

--- src/main/java/com/octopus/eventhandlers/impl/CouchDBResults.java
+++ src/main/java/com/octopus/eventhandlers/impl/CouchDBResults.java
@@ -1,16 +1,19 @@
 package com.octopus.eventhandlers.impl;
 
+import com.google.gson.Gson;
 import com.octopus.decoratorbase.AutomatedBrowserBase;
 import com.octopus.eventhandlers.EventHandler;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 import java.text.DecimalFormat;
 import java.time.Instant;
@@ -49,30 +52,72 @@ public class CouchDBResults implements EventHandler {
         }
 
         if (proceed(status, headers, COUCHDB_FAILURE_ONLY)) {
-            final String result = status ? df.format(AutomatedBrowserBase.getStaticAverageWaitTime() / 1000) : "";
+
 
             final CredentialsProvider provider = new BasicCredentialsProvider();
             final UsernamePasswordCredentials credentials
                     = new UsernamePasswordCredentials(headers.get(COUCHDB_USERNAME), headers.get(COUCHDB_PASSWORD));
             provider.setCredentials(AuthScope.ANY, credentials);
 
-            try (final CloseableHttpClient client = HttpClients.custom().setDefaultCredentialsProvider(provider).build()) {
-                final HttpPut httpPut = new HttpPut(headers.get(COUCHDB_URL) + "/" +
-                        headers.get(COUCHDB_DATABASE) + "/" +
-                        headers.get(COUCHDB_DOCUMENT));
-                httpPut.setHeader("Content-Type", "application/json");
-                final String body = "{\"Average\": " + result + ", \"Executed\":" + Instant.now().getEpochSecond() + "}";
-                httpPut.setEntity(new StringEntity(body));
-                try (final CloseableHttpResponse response = client.execute(httpPut)) {
-                    if (!(response.getStatusLine().getStatusCode() == 200 || response.getStatusLine().getStatusCode() == 201)) {
-                        throw new Exception("Failed to post to CouchDB");
-                    }
-                }
-            } catch (final Exception ex) {
-                System.out.println("Failed to send result to CouchDB.");
-            }
+            final String currentRevision = getCurrentRevision(headers, provider);
+            saveResults(status, headers, provider, currentRevision);
         }
 
         return previousResults;
     }
+
+    private void saveResults(final boolean status,
+                             final Map<String, String> headers,
+                             final CredentialsProvider provider,
+                             final String currentRevision) {
+        final String result = status ? df.format(AutomatedBrowserBase.getStaticAverageWaitTime() / 1000) : "";
+
+        try (final CloseableHttpClient client = HttpClients.custom().setDefaultCredentialsProvider(provider).build()) {
+            final HttpGet httpGet = new HttpGet(headers.get(COUCHDB_URL) + "/" +
+                    headers.get(COUCHDB_DATABASE) + "/" +
+                    headers.get(COUCHDB_DOCUMENT));
+
+            try (final CloseableHttpResponse response = client.execute(httpGet)) {
+                if (!(response.getStatusLine().getStatusCode() == 200)) {
+                    throw new Exception("Failed to post to CouchDB");
+                }
+            }
+
+            final HttpPut httpPut = new HttpPut(headers.get(COUCHDB_URL) + "/" +
+                    headers.get(COUCHDB_DATABASE) + "/" +
+                    headers.get(COUCHDB_DOCUMENT));
+            httpPut.setHeader("Content-Type", "application/json");
+            final String body = "{\"_rev\": \"" + currentRevision + "\", \"Average\": " + result + ", \"Executed\":" + Instant.now().getEpochSecond() + "}";
+            httpPut.setEntity(new StringEntity(body));
+            try (final CloseableHttpResponse response = client.execute(httpPut)) {
+                if (!(response.getStatusLine().getStatusCode() == 200 || response.getStatusLine().getStatusCode() == 201)) {
+                    throw new Exception("Failed to post to CouchDB");
+                }
+            }
+        } catch (final Exception ex) {
+            System.out.println("Failed to send result to CouchDB.");
+        }
+    }
+
+    private String getCurrentRevision(final Map<String, String> headers, final CredentialsProvider provider) {
+        try (final CloseableHttpClient client = HttpClients.custom().setDefaultCredentialsProvider(provider).build()) {
+            final HttpGet httpGet = new HttpGet(headers.get(COUCHDB_URL) + "/" +
+                    headers.get(COUCHDB_DATABASE) + "/" +
+                    headers.get(COUCHDB_DOCUMENT));
+
+            try (final CloseableHttpResponse response = client.execute(httpGet)) {
+                if (response.getStatusLine().getStatusCode() == 200) {
+                    return (new Gson()).fromJson(EntityUtils.toString(response.getEntity()), CouchDBDocument.class)._rev;
+                }
+            }
+        } catch (final Exception ex) {
+            // ignored
+        }
+
+        return null;
+    }
+}
+
+class CouchDBDocument {
+    String _rev;
 }
