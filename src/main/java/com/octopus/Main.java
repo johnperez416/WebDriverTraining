4060c19d3de42d253508954d6e5c31e93e7d99de
Matthew Casperson <matthewcasperson@gmail.com>
2019-09-13 15:11:35 +1000

    Made arguments a simple passthrough

--- src/main/java/com/octopus/Main.java
+++ src/main/java/com/octopus/Main.java
@@ -2,22 +2,17 @@ package com.octopus;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
         try {
             final ArrayList<String> options = new ArrayList<String>() {{
-                add("--monochrome");
                 add("--glue");
                 add("com.octopus.decoratorbase");
             }};
 
-            if (args.length >= 2) {
-                options.add("--tags");
-                options.add(args[1]);
-            }
-
-            options.add(args[0]);
+            Collections.addAll(options, args);
 
             cucumber.api.cli.Main.run(
                     options.toArray(new String[0]),
