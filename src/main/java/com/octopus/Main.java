45a89642d1537238eb825125ff9cc3f09324342d
Matthew Casperson <matthewcasperson@gmail.com>
2019-09-13 15:08:37 +1000

    Added ability to pass in tags

--- src/main/java/com/octopus/Main.java
+++ src/main/java/com/octopus/Main.java
@@ -1,15 +1,26 @@
 package com.octopus;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         try {
+            final ArrayList<String> options = new ArrayList<String>() {{
+                add("--monochrome");
+                add("--glue");
+                add("com.octopus.decoratorbase");
+            }};
+
+            if (args.length >= 2) {
+                options.add("--tags");
+                options.add(args[1]);
+            }
+
+            options.add(args[0]);
+
             cucumber.api.cli.Main.run(
-                    new String[]{
-                            "--monochrome",
-                            "--glue", "com.octopus.decoratorbase",
-                            args[0]},
+                    options.toArray(new String[0]),
                     Thread.currentThread().getContextClassLoader());
         } catch (IOException ex) {
             System.exit(1);
