2fbc1ce9525ba2c8edd4d98fa5e5568253704eba
Matthew Casperson <matthewcasperson@gmail.com>
2019-10-09 10:48:39 +1000

    Forced a solid border when highlighting an element

--- src/main/java/com/octopus/decorators/HighlightDecorator.java
+++ src/main/java/com/octopus/decorators/HighlightDecorator.java
@@ -58,7 +58,11 @@ public class HighlightDecorator extends AutomatedBrowserBase {
                         element);
             } else {
                 ((JavascriptExecutor) getWebDriver()).executeScript(
-                        "arguments[0].style.outline = '5px solid rgb(0, 204, 101)'; arguments[0].style['outline-offset'] = '" + offsetValue + "px';",
+                        """
+                        arguments[0].style.outline = '5px solid rgb(0, 204, 101)';
+                        arguments[0].style['outline-offset'] = '""" + offsetValue + "px" + """
+                        arguments[0].style['outline-style'] = 'solid';
+                        """,
                         element);
             }
         } catch (final TimeoutException ex) {
