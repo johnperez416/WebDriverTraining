8d35edeae0f47ff7d906c4e653dbce52ca7478de
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-26 19:16:28 +1000

    Added the ability to add a pause between steps

--- src/main/java/com/octopus/stephandlers/StepPauseHandler.java
+++ src/main/java/com/octopus/stephandlers/StepPauseHandler.java
@@ -5,6 +5,7 @@ import com.octopus.utils.SystemPropertyUtils;
 import com.octopus.utils.impl.SystemPropertyUtilsImpl;
 import io.cucumber.plugin.EventListener;
 import io.cucumber.plugin.event.EventPublisher;
+import io.cucumber.plugin.event.PickleStepTestStep;
 import io.cucumber.plugin.event.TestStepFinished;
 import io.vavr.control.Try;
 
@@ -21,7 +22,7 @@ public class StepPauseHandler implements EventListener {
     }
 
     private void handleTestStepFinished(final TestStepFinished event) {
-        if (pauseTime > 0) {
+        if (pauseTime > 0 && isActionStep(event)) {
             Try.run(() -> Thread.sleep(pauseTime * 1000));
         }
     }
@@ -29,5 +30,27 @@ public class StepPauseHandler implements EventListener {
     @Override
     public void setEventPublisher(final EventPublisher eventPublisher) {
         eventPublisher.registerHandlerFor(TestStepFinished.class, this::handleTestStepFinished);
+    }
+
+    /**
+     * Match any action steps
+     * @param event The evenmt
+     * @return true if it is an action step, and false otherwise
+     */
+    private boolean isActionStep(final TestStepFinished event) {
+        if (event.getTestStep() instanceof PickleStepTestStep) {
+            final String step =  ((PickleStepTestStep) event.getTestStep()).getStep().getText();
+            return step.contains("click") ||
+                    step.contains("populate") ||
+                    step.contains("clear") ||
+                    step.contains("select") ||
+                    step.contains("scroll") ||
+                    step.contains("zoom") ||
+                    step.contains("mouse over") ||
+                    step.contains("focus") ||
+                    step.contains("press");
+        }
+
+        return false;
     }
 }
