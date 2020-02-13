b92b81a6017b1cb777a0d001605b24dc47a14a2d
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-13 16:26:35 +1000

    Made the lambda browser use a clean environment with each instance.

--- src/main/java/com/octopus/decorators/ChromeLambdaDecorator.java
+++ src/main/java/com/octopus/decorators/ChromeLambdaDecorator.java
@@ -16,6 +16,7 @@ public class ChromeLambdaDecorator extends AutomatedBrowserBase
     private final File userData;
     private final File dataPath;
     private final File cacheDir;
+    private final File homeDir;
 
     public ChromeLambdaDecorator(final boolean headless, final AutomatedBrowser automatedBrowser) {
         super(automatedBrowser);
@@ -25,6 +26,7 @@ public class ChromeLambdaDecorator extends AutomatedBrowserBase
         userData = Files.createTempDir();
         dataPath = Files.createTempDir();
         cacheDir = Files.createTempDir();
+        homeDir = Files.createTempDir();
     }
 
     @Override
@@ -39,7 +41,7 @@ public class ChromeLambdaDecorator extends AutomatedBrowserBase
         options.addArguments("--no-sandbox");
         options.addArguments("--user-data-dir=" + userData.getAbsolutePath());
         options.addArguments("--data-path=" + dataPath.getAbsolutePath());
-        options.addArguments("--homedir=/tmp");
+        options.addArguments("--homedir=" + homeDir.getAbsolutePath());
         options.addArguments("--disk-cache-dir=" + cacheDir.getAbsolutePath());
 
         if (System.getProperty("chrome.binary") != null) {
@@ -58,6 +60,7 @@ public class ChromeLambdaDecorator extends AutomatedBrowserBase
         FileUtils.deleteQuietly(userData);
         FileUtils.deleteQuietly(dataPath);
         FileUtils.deleteQuietly(cacheDir);
+        FileUtils.deleteQuietly(homeDir);
     }
 
     @Override
