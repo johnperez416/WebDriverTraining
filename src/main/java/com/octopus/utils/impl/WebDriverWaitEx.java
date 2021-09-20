3dc7ae402a019620e9959341c41f08df730f0b1f
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-21 09:50:50 +1000

    Revert "Fixed new class requirements"
    This reverts commit 3712837e993cdeb93acb347cf4e2c7bea27657c1.

--- src/main/java/com/octopus/utils/impl/WebDriverWaitEx.java
+++ src/main/java/com/octopus/utils/impl/WebDriverWaitEx.java
@@ -1,26 +1,26 @@
 package com.octopus.utils.impl;
 
 import org.openqa.selenium.WebDriver;
-import java.time.Clock;
+import org.openqa.selenium.support.ui.Clock;
 import org.openqa.selenium.support.ui.Sleeper;
+import org.openqa.selenium.support.ui.SystemClock;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class WebDriverWaitEx extends WebDriverWait {
     public static final long DEFAULT_SLEEP_TIMEOUT = 10;
 
     public WebDriverWaitEx(final WebDriver driver, final long timeOutInSeconds) {
-        this(driver, Clock.systemDefaultZone(), Sleeper.SYSTEM_SLEEPER, timeOutInSeconds, DEFAULT_SLEEP_TIMEOUT);
+        this(driver, new SystemClock(), Sleeper.SYSTEM_SLEEPER, timeOutInSeconds, DEFAULT_SLEEP_TIMEOUT);
     }
 
     public WebDriverWaitEx(final WebDriver driver, final long timeOut, final TimeUnit time) {
-        this(driver, Clock.systemDefaultZone(), Sleeper.SYSTEM_SLEEPER, timeOut, DEFAULT_SLEEP_TIMEOUT, time);
+        this(driver, new SystemClock(), Sleeper.SYSTEM_SLEEPER, timeOut, DEFAULT_SLEEP_TIMEOUT, time);
     }
 
     public WebDriverWaitEx(final WebDriver driver, final long timeOutInSeconds, final long sleepInMillis) {
-        this(driver, Clock.systemDefaultZone(), Sleeper.SYSTEM_SLEEPER, timeOutInSeconds, sleepInMillis);
+        this(driver, new SystemClock(), Sleeper.SYSTEM_SLEEPER, timeOutInSeconds, sleepInMillis);
     }
 
     public WebDriverWaitEx(
@@ -42,6 +42,8 @@ public class WebDriverWaitEx extends WebDriverWait {
             final TimeUnit time) {
 
         // Call the WebDriverWait constructor with a timeout of 0
-        super(driver, Duration.of(timeOut, time.toChronoUnit()), Duration.ofSeconds(sleepTimeOut), clock, sleeper);
+        super(driver, clock, sleeper, 0, sleepTimeOut);
+        // Now set the timeout, possibly as a sub-second duration
+        withTimeout(timeOut, time);
     }
 }
