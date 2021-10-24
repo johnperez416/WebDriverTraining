fe6649f3f2823cddd709cbefa2a76ccb7797ffc1
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-24 16:26:23 +1000

    Added some headless browsers to the test

--- src/test/java/com/octopus/FactoryTest.java
+++ src/test/java/com/octopus/FactoryTest.java
@@ -8,36 +8,38 @@ import org.junit.runners.Parameterized;
 @RunWith(Parameterized.class)
 public class FactoryTest {
 
-    private static final AutomatedBrowserFactory AUTOMATED_BROWSER_FACTORY
-            = new AutomatedBrowserFactory();
+  private static final AutomatedBrowserFactory AUTOMATED_BROWSER_FACTORY
+      = new AutomatedBrowserFactory();
 
-    private String browser;
+  private String browser;
 
-    public FactoryTest(final String browser) {
-        this.browser = browser;
+  public FactoryTest(final String browser) {
+    this.browser = browser;
+  }
+
+  @Parameterized.Parameters
+  public static Iterable data() {
+    return Arrays.asList(
+        "FirefoxHeadless",
+        "ChromeHeadless",
+        "FirefoxNoImplicitWaitNoProxy",
+        "ChromeNoImplicitWaitNoProxy",
+        "FirefoxNoImplicitWait",
+        "Firefox",
+        "Chrome",
+        "ChromeNoImplicitWait"
+    );
+  }
+
+  @Test
+  public void openURL() {
+    final AutomatedBrowser automatedBrowser =
+        AUTOMATED_BROWSER_FACTORY.getAutomatedBrowser(browser);
+    automatedBrowser.init();
+    try {
+      automatedBrowser.goTo("https://google.com/");
+    } finally {
+      automatedBrowser.destroy();
     }
-
-    @Parameterized.Parameters
-    public static Iterable data() {
-        return Arrays.asList(
-                "FirefoxNoImplicitWaitNoProxy",
-                "ChromeNoImplicitWaitNoProxy",
-                "FirefoxNoImplicitWait",
-                "Firefox",
-                "Chrome",
-                "ChromeNoImplicitWait"
-        );
-    }
-
-    @Test
-    public void openURL() {
-        final AutomatedBrowser automatedBrowser =
-                AUTOMATED_BROWSER_FACTORY.getAutomatedBrowser(browser);
-        automatedBrowser.init();
-        try {
-            automatedBrowser.goTo("https://google.com/");
-        } finally {
-            automatedBrowser.destroy();
-        }
-    }
+  }
 }
