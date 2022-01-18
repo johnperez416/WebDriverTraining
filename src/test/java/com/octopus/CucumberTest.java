fde50122447a23e1eae498a42c58affc1a9597a4
Matthew Casperson <matthewcasperson@gmail.com>
2022-01-18 12:08:24 +1000

    Maintaining ticket monster was costing too much and so it has been shut down.

--- src/test/java/com/octopus/CucumberTest.java
+++ src/test/java/com/octopus/CucumberTest.java
@@ -1,13 +1,13 @@
 package com.octopus;
 
-import com.kevinmost.junit_retry_rule.Retry;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(glue = "com.octopus.decoratorbase")
-@Retry
+@Ignore("Ticketmonster has been shuit down")
 public class CucumberTest {
 
 }
